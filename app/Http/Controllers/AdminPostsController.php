dd4245e40f773562bd2e87c25993ac90821a01ec
陳美妤\admin <s3a432090@student.ncut.edu.tw>
2017-11-27 19:12:37 +0800

    在 AdminPostsController的 destroy內刪除資料

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -42,7 +42,11 @@ class AdminPostsController extends Controller
     return redirect()->route('admin.posts.index');
     }
 
-  
+    public function destroy($id)
+    {
+    Post::destroy($id);
+    return redirect()->route('admin.posts.index');
+    }
 
 
 }
